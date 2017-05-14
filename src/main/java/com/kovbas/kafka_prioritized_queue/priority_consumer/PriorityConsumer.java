86501a1a67746513342d44595046990dba0865a9
Ivan Kovbas <kovbas.ivan@gmail.com>
2017-05-14 23:55:36 +0300

    Refactoring: moved all functionallity related to priority polling to PriorityConsumerDecorator

---
+++ src/main/java/com/kovbas/kafka_prioritized_queue/priority_consumer/PriorityConsumer.java
@@ -0,0 +1,186 @@
+package com.kovbas.kafka_prioritized_queue.priority_consumer;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.TopicPartition;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+public class PriorityConsumer<K, V> extends AbstractConsumerDecorator<K, V> {
+
+
+    /**
+     * List of topics to consume.
+     * Order of topics in list represents topics priority.
+     */
+    List<String> topics;
+
+
+    /**
+     * Creates a PriorityConsumer with topics priority.
+     *
+     * @param consumer A Consumer Instance
+     * @param topics Topics priority.
+     */
+    public PriorityConsumer(Consumer<K, V> consumer, List<String> topics) {
+
+        super(consumer);
+
+        this.topics = topics;
+
+        consumer.subscribe(topics);
+    }
+
+
+    @Override
+    public ConsumerRecords<K, V> poll(long timeout) {
+
+        long start      = System.currentTimeMillis();
+        long remaining  = timeout;
+
+        ConsumerRecords<K, V> resultRecords = ConsumerRecords.empty();
+
+        if (consumer.assignment().isEmpty()) {
+            forcePartitionsAssignment();
+        }
+
+        do {
+
+            // Find highest priority topic with available messages
+            String topic = findTopicToRead();
+
+
+            if (topic != null) {
+
+                // poll data from specific topic and handle messages
+                pauseTopicsExcept(topic);
+                resultRecords = consumer.poll(0L);
+
+            } else {
+
+                // If there isn't topic with unpolled messages then pause
+                // all topics and call poll to not cause session timeout
+                pauseAllTopics();
+                consumer.poll(1000L); // TODO: allow to set this value from outside
+            }
+
+            resumeAllTopics();
+
+            if (!resultRecords.isEmpty()) {
+                return resultRecords;
+            }
+
+            long elapsed    = System.currentTimeMillis() - start;
+            remaining       = timeout - elapsed;
+
+        } while (remaining > 0);
+
+        return resultRecords;
+    }
+
+
+    /**
+     * Force partition assignment without changing current offsets
+     */
+    private void forcePartitionsAssignment() {
+
+        // Call poll to force partition assignment
+        ConsumerRecords<K, V> data = consumer.poll(0L);
+
+        // Reset current offset to previous position
+        data.partitions().forEach(partition ->
+
+                data.records(partition).stream()
+
+                        .findFirst().ifPresent(record -> {
+
+                            consumer.seek(partition, record.offset());
+
+                        }
+                )
+        );
+    }
+
+
+    /**
+     * Pause partitions of all topics
+     */
+    private void pauseAllTopics() {
+        consumer.pause(consumer.assignment());
+    }
+
+
+    /**
+     * Pause all topics partitions except the giving one
+     *
+     * @param topic
+     */
+    private void pauseTopicsExcept(String topic) {
+
+        Set<TopicPartition> topicPartitions = consumer.assignment().stream()
+                .filter(topicPartition -> !topicPartition.topic().equals(topic))
+                .collect(Collectors.toSet());
+
+        consumer.pause(topicPartitions);
+    }
+
+
+    /**
+     * Resume partitions of all topics
+     */
+    private void resumeAllTopics() {
+
+        consumer.resume(consumer.paused());
+    }
+
+
+    /**
+     * Returns highest priority topic with available messages
+     *
+     * @return
+     */
+    private String findTopicToRead() {
+
+        // Map topic partitions to topics
+        Map<String, List<TopicPartition>> partitionsByTopic = getPartitionsByTopicMap();
+
+        // Get end offsets of all partitions from the server
+        Map<TopicPartition, Long> endOffsets = consumer.endOffsets(consumer.assignment());
+
+        if (partitionsByTopic.isEmpty() || endOffsets.isEmpty()) {
+            return null;
+        }
+
+        // Go through topics by priority and check if there is new messages for the topic
+        for (String topic : topics) {
+
+            for (TopicPartition partition : partitionsByTopic.get(topic)) {
+
+                if (consumer.position(partition) < endOffsets.get(partition)) {
+
+                    return topic;
+                }
+
+            }
+        }
+
+        return null;
+    }
+
+
+    /**
+     * Map topic partitions to topics
+     *
+     * @return
+     */
+    private Map<String, List<TopicPartition>> getPartitionsByTopicMap() {
+
+        return consumer.assignment().stream()
+                .collect(Collectors.groupingBy(TopicPartition::topic));
+    }
+
+}
--- src/main/java/com/kovbas/kafka_prioritized_queue/tasks/ConsumerTask.java
+++ src/main/java/com/kovbas/kafka_prioritized_queue/tasks/ConsumerTask.java
@@ -1,10 +1,9 @@
 package com.kovbas.kafka_prioritized_queue.tasks;
 
+import com.kovbas.kafka_prioritized_queue.priority_consumer.PriorityConsumer;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.TopicPartition;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -12,7 +11,6 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 
 @Component
@@ -24,146 +22,28 @@ public class ConsumerTask implements ApplicationRunner {
     @Value("#{${kafka_consumer_properties}}")
     private Properties consumerProperties;
 
-    private Consumer<String, String> consumer;
-
 
     @Override
     @Async
     public void run(ApplicationArguments applicationArguments) throws Exception {
 
-        consumer = new KafkaConsumer<>(consumerProperties);
+        Consumer<String, String> consumer = new KafkaConsumer<>(consumerProperties);
+
+        consumer = new PriorityConsumer<>(consumer, topics);
+
 
         try {
 
-            consumer.subscribe(topics);
-
-            forcePartitionsAssignment();
-
             while (true) {
 
-                // Find highest priority topic with available messages
-                String topic = findTopicToRead();
-
-
-                if (topic != null) {
-
-                    // poll data from specific topic and handle messages
-                    pauseTopicsExcept(topic);
-                    consumer.poll(0L).forEach(this::handleRecord);
-
-                } else {
-
-                    // If there isn't topic with unpolled messages then pause
-                    // all topics and call poll to not cause session timeout
-                    pauseAllTopics();
-                    consumer.poll(1000L);
-
-                }
-
-                resumeAllTopics();
+                consumer.poll(Long.MAX_VALUE).forEach(this::handleRecord);
             }
 
         } finally {
+
             consumer.close();
         }
 
-    }
-
-
-    /**
-     * Force partition assignment without changing current offsets
-     */
-    private void forcePartitionsAssignment() {
-
-        // Call poll to force partition assignment
-        ConsumerRecords<String, String> data = consumer.poll(0L);
-
-        // Reset current offset to previous position
-        data.partitions().forEach(partition ->
-
-                data.records(partition).stream()
-
-                        .findFirst().ifPresent(record -> {
-
-                            consumer.seek(partition, record.offset());
-
-                        }
-                )
-        );
-    }
-
-
-    private void pauseAllTopics() {
-        consumer.pause(consumer.assignment());
-    }
-
-    /**
-     * Pause all topics partitions except the giving one
-     *
-     * @param topic
-     */
-    private void pauseTopicsExcept(String topic) {
-
-        Set<TopicPartition> topicPartitions = consumer.assignment().stream()
-                .filter(topicPartition -> !topicPartition.topic().equals(topic))
-                .collect(Collectors.toSet());
-
-        consumer.pause(topicPartitions);
-    }
-
-
-    /**
-     * Resume partitions of all topics
-     */
-    private void resumeAllTopics() {
-
-        consumer.resume(consumer.paused());
-    }
-
-
-    /**
-     * Returns highest priority topic with available messages
-     *
-     * @return
-     */
-    private String findTopicToRead() {
-
-        // Map topic partitions to topics
-        Map<String, List<TopicPartition>> partitionsByTopic = getPartitionsByTopicMap();
-
-        // Get end offsets of all partitions from the server
-        Map<TopicPartition, Long> endOffsets = consumer.endOffsets(consumer.assignment());
-
-        if (partitionsByTopic.isEmpty() || endOffsets.isEmpty()) {
-            return null;
-        }
-
-        // Go through topics by priority and check if there is new messages for the topic
-        for (String topic : topics) {
-
-            for (TopicPartition partition : partitionsByTopic.get(topic)) {
-
-                if (consumer.position(partition) < endOffsets.get(partition)) {
-
-                    return topic;
-                }
-
-            }
-        }
-
-        return null;
-    }
-
-
-    /**
-     * Map topic partitions to topics
-     *
-     * @return
-     */
-    private Map<String, List<TopicPartition>> getPartitionsByTopicMap() {
-
-        return consumer.assignment().stream()
-                .collect(Collectors.groupingBy(TopicPartition::topic));
     }
 
 
